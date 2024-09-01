80d36471651699b16bb62bce2819d2deafa45263
Morozov Dmitriy <jimmoroz@gmail.com>
2024-09-01 14:14:38 +0300

    feat(email-sender): отправка отчетов по задачам

--- task-tracker-backend/email-sender/src/main/java/com/tasktracker/service/TaskReportConsumer.java
+++ task-tracker-backend/email-sender/src/main/java/com/tasktracker/service/TaskReportConsumer.java
@@ -7,19 +7,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-public class RegistrationConsumer {
+public class TaskReportConsumer {
     private final EmailSender emailSender;
 
-    public RegistrationConsumer(EmailSender emailSender) {
+    public TaskReportConsumer(EmailSender emailSender) {
         this.emailSender = emailSender;
     }
 
-    @KafkaListener(topics = "registration", groupId = "registration_group")
+    @KafkaListener(topics = "tasks", groupId = "tasks_group")
     void listener(MessageDto message) {
         try {
-            log.info("Received message [{}] in registration_group", message);
+            log.info("Received message [{}] in tasks_group", message);
             emailSender.sendSimpleEmail(message);
-            log.info("Sent message [{}] in registration_group", message);
+            log.info("Sent message [{}] in tasks_group", message);
         } catch (Exception e) {
             log.error(e.getMessage());
         }
